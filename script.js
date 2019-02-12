52bcdc5e145982a8cf3f599f5ecfa87174ee4f87
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-02-12 19:49:02 +1300

    Update script.js

--- script.js
+++ script.js
@@ -1,95 +1,96 @@
 document.onkeydown = function(e) {
-    if (e.ctrlKey && e.which == 70) {
-      showfacade();
-    }
+  if (e.ctrlKey && e.which == 70) {
+    showfacade();
+  }
+};
 
 
-    function delacces() {
-      var ls = localStorage.getItem("pass");
-      localStorage.removeItem("pass");
-      window.location.replace("/46-41-43-41-44-45/");
-    }
-    var ls = localStorage.getItem("pass");
-    if (ls == null) {
-      window.location.replace("/46-41-43-41-44-45/");
-    }
+function delacces() {
+  var ls = localStorage.getItem("pass");
+  localStorage.removeItem("pass");
+  window.location.replace("/46-41-43-41-44-45/");
+}
+var ls = localStorage.getItem("pass");
+if (ls == null) {
+  window.location.replace("/46-41-43-41-44-45/");
+}
 
-    var ls = localStorage.getItem("pass");
-    if (ls == "no") {
-      window.location.replace("/46-41-43-41-44-45/");
-    }
+var ls = localStorage.getItem("pass");
+if (ls == "no") {
+  window.location.replace("/46-41-43-41-44-45/");
+}
 
-    //make sure that it doesn't reaload 
-    window.onbeforeunload = function() {
-      return " are you sure! you want to leave this page";
-    }
-    setTimeout(function() {
-      //checking for default facade
-      var d = localStorage.getItem("defaultFacade");
-      if (d == null) {
-        var DF = prompt("DEFAULT FACADE\n1=classrooms\n2=MathsWhizz\n3=Google\n4=sketchnation\n5=newtab", "");
-        localStorage.setItem("defaultFacade", DF);
-        $.notify("Changed Default Facade", "success");
-      }
-      //Change Title
-      var t = prompt("Title");
-      document.title = t;
-      console.log(t);
-      //Change favicon
-      var NF = prompt("link for page favicon =", "");
-      var NF = "https://www.google.com/s2/favicons?domain=" + NF;
-      (function() {
-        var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
-        link.href = NF;
-        document.getElementsByTagName('head')[0].appendChild(link);
-      })();
-    }, 3000);
-    //button changing title/favicon
-    function CT() {
-      var t = prompt("Title");
-      document.title = t;
-      document.getElementsByTagName("button")[0].setAttribute("title", t);
-      console.log(t);
-      var NF = prompt("link for page favicon =", "");
-      var NF = "https://www.google.com/s2/favicons?domain=" + NF;
-      (function() {
-        var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
-        link.type = 'image/x-icon';
-        link.rel = 'shortcut icon';
-        link.href = NF;
-        document.getElementsByTagName('head')[0].appendChild(link);
-      })();
-      if (t == null) {
-        $.notify("An Error occurred Changing The Title", "error");
-      } else {
-        $.notify("Changed Title", "success");
-      }
-      if (NF == "https://www.google.com/s2/favicons?domain=null") {
-        $.notify("An Error occurred Changing The Favicon", "error");
-      } else {
-        $.notify("Changed Favicon", "success");
-      }
-    }
-    //password
-    "Password = HeHe"
-    //console
-    function consol() {
-      $.notify("Login", "info");
-      console.log("CONSOLE");
-      var cpw = prompt("Type In The Password");
-      if (cpw == "HaPaRa") {
-        $.notify("Access granted", "success");
-      } else {
-        $.notify("INCORRECT PASSWORD", "error");
-      }
-    }
-    //changing the page you want to go to
-    function AP() {
-      var cp = prompt("What page do you want to go to?", "https://example.com");
-      document.getElementsByTagName("iframe")[0].setAttribute("src", cp);
-      var ss = "https://www.google.com/s2/favicons?domain=" + cp;
-      document.getElementsByTagName("img")[0].setAttribute("src", ss);
-      $.notify("Chosen Page " + cp, "success");
-    }
+//make sure that it doesn't reaload 
+window.onbeforeunload = function() {
+  return " are you sure! you want to leave this page";
+}
+setTimeout(function() {
+  //checking for default facade
+  var d = localStorage.getItem("defaultFacade");
+  if (d == null) {
+    var DF = prompt("DEFAULT FACADE\n1=classrooms\n2=MathsWhizz\n3=Google\n4=sketchnation\n5=newtab", "");
+    localStorage.setItem("defaultFacade", DF);
+    $.notify("Changed Default Facade", "success");
+  }
+  //Change Title
+  var t = prompt("Title");
+  document.title = t;
+  console.log(t);
+  //Change favicon
+  var NF = prompt("link for page favicon =", "");
+  var NF = "https://www.google.com/s2/favicons?domain=" + NF;
+  (function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = NF;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  })();
+}, 3000);
+//button changing title/favicon
+function CT() {
+  var t = prompt("Title");
+  document.title = t;
+  document.getElementsByTagName("button")[0].setAttribute("title", t);
+  console.log(t);
+  var NF = prompt("link for page favicon =", "");
+  var NF = "https://www.google.com/s2/favicons?domain=" + NF;
+  (function() {
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = NF;
+    document.getElementsByTagName('head')[0].appendChild(link);
+  })();
+  if (t == null) {
+    $.notify("An Error occurred Changing The Title", "error");
+  } else {
+    $.notify("Changed Title", "success");
+  }
+  if (NF == "https://www.google.com/s2/favicons?domain=null") {
+    $.notify("An Error occurred Changing The Favicon", "error");
+  } else {
+    $.notify("Changed Favicon", "success");
+  }
+}
+//password
+"Password = HeHe"
+//console
+function consol() {
+  $.notify("Login", "info");
+  console.log("CONSOLE");
+  var cpw = prompt("Type In The Password");
+  if (cpw == "HaPaRa") {
+    $.notify("Access granted", "success");
+  } else {
+    $.notify("INCORRECT PASSWORD", "error");
+  }
+}
+//changing the page you want to go to
+function AP() {
+  var cp = prompt("What page do you want to go to?", "https://example.com");
+  document.getElementsByTagName("iframe")[0].setAttribute("src", cp);
+  var ss = "https://www.google.com/s2/favicons?domain=" + cp;
+  document.getElementsByTagName("img")[0].setAttribute("src", ss);
+  $.notify("Chosen Page " + cp, "success");
+}
