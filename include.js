3b07205c189e8b3e13b27ccd39fc1210280b36a1
TexRexTex <40045439+TexRexTex@users.noreply.github.com>
2019-12-24 14:04:09 +1300

    Update include.js

--- include.js
+++ include.js
@@ -1,3 +1,5 @@
+var data = "";
+
 function getinclude(){
 var d = localStorage.getItem("defaultFacade");
 var include;
@@ -33,31 +35,24 @@ document.getElementById("include").src=include;
 }
 
 
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /*loop through a collection of all HTML elements:*/
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /*make an HTTP request using the attribute value as the file name:*/
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /*remove the attribute, and call this function once more:*/
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }      
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /*exit the function:*/
-      return;
+function xhttpGet(url, REPLACEMENTID) {
+  data = "";
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      if (REPLACEMENTID) {
+        document.getElementById(REPLACEMENTID).innerHTML = xhttp.responseText;
+      } else {
+        document.body.innerHTML = xhttp.responseText;
+      }
     }
-  }
-};
+  };
+  xhttp.open("GET", "https://cors-anywhere.herokuapp.com/" + url, true);
+  xhttp.send();
+}
+
+
+
+
+
 getinclude();
